84a2054b119e48828c37d27660194291ca6df580
AngelicaLangarica <al@lucion.mx>
2025-03-06 16:34:17 -0600

    cambios para implementar plazas y direcciones de entrega

--- models/account_move.py
+++ models/account_move.py
@@ -9,19 +9,19 @@ class AccountMovePlazas(models.Model):
 
     plaza_id = fields.Many2one('plazas.manager', string="Plaza", tracking=True)
 
-    @api.onchange('partner_id','partner_shipping_id')
-    def _change_partner_plaza(self):
-        if self.partner_id:
-            self.plaza_id = self.partner_shipping_id.plaza_id.id
+    # @api.onchange('partner_id','partner_shipping_id')
+    # def _change_partner_plaza(self):
+    #     if self.partner_id:
+    #         self.plaza_id = self.partner_shipping_id.plaza_id.id
         
 
-    @api.onchange('partner_id')
-    def _set_related_partners(self):
-        shipping_partner = self.plaza_id and self.env['res.partner'].sudo().search([('parent_id', '=', self.plaza_id.id),('type', 'in', ['delivery'])])
-        shipping_domain = []
-        res = {}
-        if shipping_partner:
-            shipping_domain = expression.AND([shipping_domain, [('id', 'in', shipping_partner.mapped('id'))]])
+    # @api.onchange('partner_id')
+    # def _set_related_partners(self):
+    #     shipping_partner = self.plaza_id and self.env['res.partner'].sudo().search([('parent_id', '=', self.plaza_id.id),('type', 'in', ['delivery'])])
+    #     shipping_domain = []
+    #     res = {}
+    #     if shipping_partner:
+    #         shipping_domain = expression.AND([shipping_domain, [('id', 'in', shipping_partner.mapped('id'))]])
 
-        res['domain'] = {'partner_shipping_id': shipping_domain}
-        return res
+    #     res['domain'] = {'partner_shipping_id': shipping_domain}
+    #     return res
