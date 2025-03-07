3fb0e9e7e2332a9960c736c28933ba7dd883ff4f
Hector Chavez <chavezcortez298@gmail.com>
2025-03-06 23:29:53 -0600

    Add Field Plaza account.move

--- models/account_move.py
+++ models/account_move.py
@@ -3,21 +3,33 @@
 
 from flectra import models, fields, api, _
 from flectra.osv import expression
+import logging
 
+_logger = logging.getLogger(__name__)
 class AccountMovePlazas(models.Model):
     _inherit = 'account.move'
 
-    plaza_id = fields.Many2one('plazas.manager', string="Plaza", tracking=True)
-
-    @api.onchange('partner_id','partner_shipping_id')
-    def _change_partner_plaza(self):
+    def set_default_plaza(self):
+        _logger.info(f"Its Working {self.partner_shipping_id}")
         if self.partner_id:
-            self.plaza_id = self.partner_shipping_id.plaza_id.id
-        
+            return self.partner_shipping_id.plaza_id    
+        return self.partner_shipping_id.plaza_id
+
+    plaza_id = fields.Many2one('plazas.manager', string="Plaza", tracking=True, default=set_default_plaza)
+
+    @api.constrains('partner_id', 'partner_shipping_id')
+    def _onchange_partner(self):
+        for rec in self:
+            _logger.info(f"Shipping Partner {rec.partner_id}----------{rec.partner_shipping_id.name}-----{rec.partner_id.name}-----{rec.plaza_id.name}")
+            if rec.partner_shipping_id:
+                rec.plaza_id = rec.partner_shipping_id.plaza_id
+            else:
+                _logger.info(f"otros------->")
+                rec.plaza_id = False
 
     @api.onchange('partner_id')
     def _set_related_partners(self):
-        shipping_partner = self.plaza_id and self.env['res.partner'].sudo().search([('parent_id', '=', self.plaza_id.id),('type', 'in', ['delivery'])])
+        shipping_partner = self.plaza_id and self.env['res.partner'].sudo().search([('plaza_id', '=', self.plaza_id.id),('type', 'in', ['delivery'])])
         shipping_domain = []
         res = {}
         if shipping_partner:
